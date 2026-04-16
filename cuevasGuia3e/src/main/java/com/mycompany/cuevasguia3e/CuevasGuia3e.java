eb6a41d055dd920c90f16f63b91e66558b710d1c
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-15 23:08:52 -0600

    Add files via upload

---
+++ cuevasGuia3e/src/main/java/com/mycompany/cuevasguia3e/CuevasGuia3e.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cuevasguia3e;
+
+/**
+ *
+ * @author carlo
+ */
+public class CuevasGuia3e {
+
+    public static void main(String[] args) {
+        new w().setVisible(true);
+    }
+}
